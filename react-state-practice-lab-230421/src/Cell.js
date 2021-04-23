691cf20d0cb26705b7904758d9cf227df6e467e5
github username <github email address>
2021-04-23 13:22:52 +0100

    created Cell component, set default row values

---
+++ react-state-practice-lab-230421/src/Cell.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cell = props => {
+  render () {
+  return 
+  }
+}
+
+export default Cell
--- react-state-practice-lab-230421/src/Matrix.js
+++ react-state-practice-lab-230421/src/Matrix.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import pattern1 from './data'
+import Cell from './Cell'
 
 export default class Matrix extends Component {
   genRow = vals => {
@@ -6,9 +8,15 @@ export default class Matrix extends Component {
   }
 
   genMatrix = () => {
-    return this.props.values.map(rowVals => (
-      <div className='row'>{this.genRow(rowVals)}</div>
-    ))
+    var matrixRows
+    this.props.values
+      ? (matrixRows = this.props.values.map(rowVals => (
+          <div className='row'>{this.genRow(rowVals)}</div>
+        )))
+      : (matrixRows = pattern1.map(rowVals => (
+          <div className='row'>{this.genRow(rowVals)}</div>
+        )))
+    return matrixRows
   }
 
   render () {
