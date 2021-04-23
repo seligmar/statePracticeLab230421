59115bdeee1be207bbf83dc79af0e6545573b80f
github username <github email address>
2021-04-23 14:20:08 +0100

    added colours to cells and created function to change colours on click

--- react-state-practice-lab-230421/src/Cell.js
+++ react-state-practice-lab-230421/src/Cell.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const Cell = props => {
+class Cell extends React.Component {
+  state = {
+    cellColour: this.props.value
+  }
+
+  changeColour = () => {
+    this.setState({ cellColour: '#FF69B4' })
+  }
+
   render () {
-  return 
+    console.log(this.props)
+    var colour = this.state.cellColour
+    return (
+      <div
+        className='cell'
+        style={{ backgroundColor: colour }}
+        onClick={this.changeColour}
+      ></div>
+    )
   }
 }
 
--- react-state-practice-lab-230421/src/Matrix.js
+++ react-state-practice-lab-230421/src/Matrix.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
-import pattern1 from './data'
 import Cell from './Cell'
 
 export default class Matrix extends Component {
   genRow = vals => {
-    return vals.map(val => <div className='cell'></div>) // replace me and render a cell component instead!
+    return vals.map(val => (
+      <Cell key={vals.indexOf(val) + Math.random()} value={val} />
+    ))
   }
 
   genMatrix = () => {
     var matrixRows
+    var defaultArray = [
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00',
+      '#F00'
+    ]
     this.props.values
       ? (matrixRows = this.props.values.map(rowVals => (
-          <div className='row'>{this.genRow(rowVals)}</div>
+          <div className='row' key={this.props.values.indexOf(rowVals) + 1 * 3}>
+            {this.genRow(rowVals)}
+          </div>
         )))
-      : (matrixRows = pattern1.map(rowVals => (
-          <div className='row'>{this.genRow(rowVals)}</div>
+      : (matrixRows = defaultArray.map(rowVals => (
+          <div className='row' key={defaultArray.indexOf(rowVals)}>
+            {this.genRow(rowVals)}
+          </div>
         )))
     return matrixRows
   }
